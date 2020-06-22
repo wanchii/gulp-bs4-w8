76005b117accb22db62428a7639f8b16c26bad03
Emily Chen <wanchichen88@gmail.com>
2020-06-22 16:34:49 +0800

    searchBar下拉選單

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -2,21 +2,7 @@
 
 $(document).ready(function () {
   console.log('Hello Bootstrap4');
-}); // var swiper = new Swiper('.swiper-container', {
-//   slidesPerView: 4,
-//   spaceBetween: 30,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//     nextEl: '.swiper-btn-next',
-//     prevEl: '.swiper-btn-prev',
-//   },
-// });
-
+});
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 4,
   spaceBetween: 30,
